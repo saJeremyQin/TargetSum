5ec65c846ca8c2265669b7f94c9d5ef45b943b18
saJeremyQin <343220868@qq.com>
2024-12-15 19:16:00 +1030

    handlePress is added

--- src/components/RandomNumber.tsx
+++ src/components/RandomNumber.tsx
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { Pressable, StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 
 interface RandomNumberProps {
@@ -8,19 +8,26 @@ interface RandomNumberProps {
 
 // export default function RandomNumber({number}: RandomNumberProps): React.JSX.Element {
 const RandomNumber: React.FC<RandomNumberProps> = ({number, backgroundColor="#3c9"}) => {
-  return (
-      <Text style={[styles.numberBox, {backgroundColor}]}>{number}</Text>
-  )    
+    const handlePress = () =>{
+        console.log(`${number} is clicked`);
+    }
+    return (
+        <Pressable onPress={handlePress} style={styles.pressable}>
+            <Text style={[styles.numberBox, {backgroundColor}]}>{number}</Text>
+        </Pressable>   
+    )    
 }
 
 const styles = StyleSheet.create({
+    pressable:{
+        margin:10
+    },
     numberBox:{
         fontSize:24,
         width:150,
-        // backgroundColor:"#0f0",
-        marginHorizontal:15,
-        marginVertical: 15,
-        textAlign:"center"
+        height:50,              //When <Text> is nested in <Pressable>, need clear size to paint it.
+        textAlign:"center",
+        paddingVertical:10,
     }  
 })
 
